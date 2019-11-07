d86907cbcdcb51fe854d5bde203d8f4d46c40887
github username <github email address>
2019-11-06 22:20:04 -0700

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 export default class ColorBox extends Component {
 
   render() {
-    return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
+    return(
+      <div className="colorbox">
+        console.log({this.value.opacity})
+        {this.value.opacity >= .02 ? <ColorBox opacity={this.value.opacity- .01}/> : null}
       </div>
+
     )
   }
-
 }
